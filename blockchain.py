2e1709680edae57913c8b62099f9c8461c073b7e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-06 02:06:06 +0300

    add quit func.

--- blockchain.py
+++ blockchain.py
@@ -35,6 +35,7 @@ while True:
     print("Please choose: ")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
+    print("q: quit")
     user_choice = get_user_choice()
 
     if user_choice == "1":
@@ -42,6 +43,8 @@ while True:
         add_value(tx_amount, get_last_blockchain_value())
     elif user_choice == "2":
         print_blockchain_elements()
+    elif user_choice == "q":
+        break
     else:
         print("Input was invalid, please pick a value from the list!")
 
