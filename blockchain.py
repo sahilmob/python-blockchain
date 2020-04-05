62965687016344ccc59af44cc76b344158e591e6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-06 02:00:38 +0300

    if-else

--- blockchain.py
+++ blockchain.py
@@ -11,18 +11,34 @@ def add_value(transaction_amount, last_transation=[1.0]):
     print(blockchain)
 
 
-def get_user_input():
-    return float(input("Your transcation amount please: "))
+def get_transaction_value():
+    user_input = float(input("Your transcation amount please: "))
+    return user_input
 
 
-tx_amount = get_user_input()
+def get_user_choice():
+    user_input = input("Your choice: ")
+    return user_input
+
+
+def print_blockchain_elements():
+    for block in blockchain:
+        print("----------")
+        print(block)
+
+
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
 
 while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
+    print("Please choose: ")
+    print("1: Add a new transaction value")
+    print("2: Output the blockchain blocks")
+    user_choice = get_user_choice()
 
-    for block in blockchain:
-        print("----------")
-        print(block)
+    if user_choice == "1":
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
