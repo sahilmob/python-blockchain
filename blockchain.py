776a6020f38655471d10cc8c8c61c90e57c19f82
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-06 16:42:13 +0300

    use the range function to iterate

--- blockchain.py
+++ blockchain.py
@@ -32,18 +32,16 @@ def print_blockchain_elements():
 
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        if block[0] == blockchain[block_index - 1]:
+        if blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
     return is_valid
 
 
@@ -70,6 +68,7 @@ while waiting_for_input:
     else:
         print("Input was invalid, please pick a value from the list!")
     if not verify_chain():
+        print_blockchain_elements()
         print("invalid blockchain")
         break
 else:
