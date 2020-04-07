04d95012a15bfc84a18c0e678a59780171506c2a
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-07 21:56:32 +0300

    add proof of work

--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,8 @@ MINING_REWARD = 10
 genesis_block = {
     "previous_hash": "",
     "index": 0,
-    "transactions": []
+    "transactions": [],
+    "proof": 100
 }
 blockchain = [genesis_block]
 open_transactions = []
@@ -17,6 +18,22 @@ participants = {"Sahil"}
 
 def hash_block(block):
     return sha256(dumps(block).encode()).hexdigest()
+
+
+def valid_proof(transactions, last_hash, proof):
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = sha256(guess).hexdigest()
+    print(guess_hash)
+    return guess_hash[0:2] == "00"
+
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block((last_block))
+    proof = 0
+    while not valid_proof(open_transactions, last_hash, proof):
+        proof += 1
+    return proof
 
 
 def get_balance(participant):
@@ -63,6 +80,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    proof = proof_of_work()
     reward_transaction = {
         "sender": "MINING",
         "recipient": owner,
@@ -73,7 +91,8 @@ def mine_block():
     block = {
         "previous_hash": hashed_block,
         "index": len(blockchain),
-        "transactions": copied_transactions
+        "transactions": copied_transactions,
+        "proof": proof
     }
     blockchain.append(block)
     return True
@@ -103,6 +122,10 @@ def verify_chain():
             continue
         if block["previous_hash"] != hash_block(blockchain[index - 1]):
             return False
+        if not valid_proof(block["transactions"][:-1], block["previous_hash"], block["proof"]):
+            print("Proof of work is invalid")
+            return False
+
     return True
 
 
