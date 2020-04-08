ebd4c98c33ab0cb4fd2053116b2834baa31e2e66
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-09 02:51:33 +0300

    use private atts.

--- blockchain.py
+++ blockchain.py
@@ -13,18 +13,24 @@ class Blockchain:
     def __init__(self, hosting_node_id):
         genesis_block = Block(0, "", [], 100, 0)
 
-        self.chain = [genesis_block]
-        self.open_transactions = []
+        self.__chain = [genesis_block]
+        self.__open_transactions = []
         self.hosting_node_id = hosting_node_id
 
         self.load_data()
+
+    def get_chain(self):
+        return self.__chain[:]
+
+    def get_open_transactions(self):
+        return self.__open_transactions[:]
 
     def load_data(self):
         try:
             with open("blockchain.txt", mode="r") as f:
                 file_content = f.readlines()
                 blockchain = loads(file_content[0][:-1])
-                self.chain = [Block(
+                self.__chain = [Block(
                     block["index"],
                     block["previous_hash"],
                     [
@@ -34,9 +40,9 @@ class Blockchain:
                     block["proof"],
                     block["timestamp"]
                 ) for block in blockchain]
-                self.open_transactions = loads(file_content[1])
-                self.open_transactions = [Transaction(
-                    tx["sender"], tx["recipient"], tx["amount"]) for tx in self.open_transactions]
+                self.__open_transactions = loads(file_content[1])
+                self.__open_transactions = [Transaction(
+                    tx["sender"], tx["recipient"], tx["amount"]) for tx in self.__open_transactions]
         except (IOError, IndexError):
             pass
 
@@ -46,29 +52,30 @@ class Blockchain:
                 f.write(dumps([block.__dict__ for block in
                                [Block(block_el.index, block_el.previous_hash, [
                                 tx.__dict__ for tx in block_el.transactions
-                                ], block_el.proof, block_el.timestamp) for block_el in self.chain]
+                                ], block_el.proof, block_el.timestamp) for block_el in self.__chain]
                                ]))
                 f.write("\n")
-                f.write(dumps([tx.__dict__ for tx in self.open_transactions]))
+                f.write(
+                    dumps([tx.__dict__ for tx in self.__open_transactions]))
         except IOError:
             print("Saving failed")
 
     def proof_of_work(self):
-        last_block = self.chain[-1]
+        last_block = self.__chain[-1]
         last_hash = hash_block((last_block))
         proof = 0
-        while not Verification.valid_proof(self.open_transactions, last_hash, proof):
+        while not Verification.valid_proof(self.__open_transactions, last_hash, proof):
             proof += 1
         return proof
 
     def get_balance(self):
         tx_sender = [[tx.amount for tx in block.transactions
-                      if tx.sender == self.hosting_node_id] for block in self.chain]
+                      if tx.sender == self.hosting_node_id] for block in self.__chain]
         open_tx_sender = [
-            tx.amount for tx in self.open_transactions if tx.sender == self.hosting_node_id]
+            tx.amount for tx in self.__open_transactions if tx.sender == self.hosting_node_id]
         tx_sender.append(open_tx_sender)
         tx_recipient = [[tx.amount for tx in block.transactions
-                         if tx.recipient == self.hosting_node_id] for block in self.chain]
+                         if tx.recipient == self.hosting_node_id] for block in self.__chain]
         amount_sent = reduce(
             lambda tx_sum, tx_current: tx_sum + sum(tx_current) if len(tx_current) > 0 else tx_sum + 0, tx_sender, 0)
         amount_recivied = reduce(
@@ -77,29 +84,29 @@ class Blockchain:
 
     def get_last_blockchain_value(self):
         """ Returns the last transaction in the blockchain """
-        if len(self.chain) < 1:
+        if len(self.__chain) < 1:
             return None
-        return self.chain[-1]
+        return self.__chain[-1]
 
     def add_transaction(self, recipient, sender, amount=1.0):
         transaction = Transaction(sender, recipient, amount)
         if Verification.verify_transaction(transaction, self.get_balance):
-            self.open_transactions.append(transaction)
+            self.__open_transactions.append(transaction)
             self.save_data()
             return True
         return False
 
     def mine_block(self, node):
-        last_block = self.chain[-1]
+        last_block = self.__chain[-1]
         hashed_block = hash_block(last_block)
         proof = self.proof_of_work()
         reward_transaction = Transaction(
             "MINING", self.hosting_node_id, MINING_REWARD)
-        copied_transactions = self.open_transactions[:]
+        copied_transactions = self.__open_transactions[:]
         copied_transactions.append(reward_transaction)
-        block = Block(len(self.chain), hashed_block,
+        block = Block(len(self.__chain), hashed_block,
                       copied_transactions, proof)
-        self.chain.append(block)
-        self.open_transactions = []
+        self.__chain.append(block)
+        self.__open_transactions = []
         self.save_data()
         return True
--- node.py
+++ node.py
@@ -21,7 +21,7 @@ class Node:
         return user_input
 
     def print_blockchain_elements(self):
-        for block in self.blockchain.chain:
+        for block in self.blockchain.get_chain():
             print(block)
         else:
             print("-" * 20)
@@ -48,7 +48,7 @@ class Node:
             elif user_choice == "3":
                 self.print_blockchain_elements()
             elif user_choice == "4":
-                if Verification.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print("All transactions are valid")
                 else:
                     print("There are invalid transactions")
@@ -56,7 +56,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print("Input was invalid, please pick a value from the list!")
-            if not Verification.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_chain()):
                 self.print_blockchain_elements()
                 print("invalid blockchain")
                 break
