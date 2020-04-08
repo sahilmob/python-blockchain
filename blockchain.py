7270b8b1e21ae2f732e0ab5d046fb9016435c4fc
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-09 00:19:14 +0300

    remove output participants from UI

--- blockchain.py
+++ blockchain.py
@@ -162,8 +162,7 @@ while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
-    print("4: Output the blockchain participants")
-    print("5: Check transactions validity")
+    print("4: Check transactions validity")
     print("q: Quit")
     user_choice = get_user_choice()
 
@@ -181,8 +180,6 @@ while waiting_for_input:
     elif user_choice == "3":
         print_blockchain_elements()
     elif user_choice == "4":
-        print(participants)
-    elif user_choice == "5":
         if verify_transactions():
             print("All transactions are valid")
         else:
