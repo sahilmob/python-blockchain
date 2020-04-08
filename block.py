92f8d2fa570510feaeac805cd5cc037cbd956875
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-09 00:30:12 +0300

    create printable blockchain useing inheritance

--- block.py
+++ block.py
@@ -1,10 +1,15 @@
 from time import time
 
+from printable import Printable
 
-class Block:
+
+class Block(Printable):
     def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
         self.index = index
         self.previous_hash = previous_hash
         self.transactions = transactions
         self.proof = proof
         self.timestamp = time() if timestamp is None else timestamp
+
+    def __repr__(self):
+        return str(self.__dict__)
