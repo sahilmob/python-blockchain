d31caca72b0cc691a3fdadd48492e1b26864b8d7
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-04-09 22:36:23 +0300

    imporve verification

--- blockchain.py
+++ blockchain.py
@@ -98,8 +98,6 @@ class Blockchain:
         if self.hosting_node_id == None:
             return False
         transaction = Transaction(sender, recipient, signature, amount)
-        if not Wallet.verify_transaction(transaction):
-            return False
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
@@ -115,12 +113,12 @@ class Blockchain:
         reward_transaction = Transaction(
             "MINING", self.hosting_node_id, "", MINING_REWARD)
         copied_transactions = self.__open_transactions[:]
+        for tx in copied_transactions:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transactions.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block,
                       copied_transactions, proof)
-        for tx in block.transactions:
-            if not Wallet.verify_transaction(tx):
-                return False
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
